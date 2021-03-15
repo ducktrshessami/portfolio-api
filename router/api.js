c6a067414c764eeb1ccb5cdfcc982a0158e6b669
Josh Fish <jwee761@gmail.com>
2021-03-15 16:56:41 -0700

    base api routes

--- router/api.js
+++ router/api.js
@@ -1,3 +1,20 @@
-module.exports = function (app) {
+const auth = require("../middleware/auth");
 
+module.exports = function (app) {
+    app.get("/projects", function (req, res) {
+
+    });
+
+    app.get("/message", function (req, res) {
+        if (auth(req.query.key)) {
+            
+        }
+        else {
+            res.status(401).end();
+        }
+    });
+
+    app.post("/message", function (req, res) {
+
+    });
 };
