4bacf6ebb65831e154f9dc91ba861e59c67ffc42
Josh Fish <jwee761@gmail.com>
2021-03-15 15:26:01 -0700

    configure server to use dotenv

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+try {
+    require("dotenv").config();
+}
+catch {
+    console.log("Not using dotenv. Make sure environment variables are set");
+}
+
 const express = require("express");
 const cors = require("cors");
 
