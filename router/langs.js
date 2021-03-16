4e1ebb0f1ebb963c031054ab298c44e1229a8cf0
ducktrshessami <jwee761@gmail.com>
2021-03-15 22:40:56 -0700

    lang list to map conversion

--- router/langs.js
+++ router/langs.js
@@ -4,7 +4,9 @@ module.exports = function (router) {
     router.get("/langs", function (req, res) {
         db.Lang.findAll()
             .then(data => {
-                
+                let foo = {};
+                data.forEach(lang => foo[lang.name] = lang.image);
+                return foo;
             })
             .then(data => res.status(200).json(data))
             .catch(err => {
