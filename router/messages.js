8b076858c3bf5aaaa4f249b16a87a0fc4654cb73
ducktrshessami <jwee761@gmail.com>
2021-03-15 20:05:33 -0700

    handle message posting

--- router/messages.js
+++ router/messages.js
@@ -7,7 +7,12 @@ module.exports = function (router) {
     });
 
     router.post("/messages", function (req, res) {
-
+        db.Message.create(req.body)
+            .then(data => res.status(200).json(data))
+            .catch(err => {
+                console.error(err);
+                res.status(500).end();
+            });
     });
 
     router.delete("/messages/:id", auth, function (req, res) {
