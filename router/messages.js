9440420aaa459d83fb78547b75368047e9641056
ducktrshessami <jwee761@gmail.com>
2021-03-15 20:19:14 -0700

    handle get messages

--- router/messages.js
+++ router/messages.js
@@ -3,7 +3,12 @@ const auth = require("../middleware/auth");
 
 module.exports = function (router) {
     router.get("/messages", auth, function (req, res) {
-
+        db.Message.findAll({ order: [["createdAt", "asc"]] })
+            .then(data => res.status(200).json(data))
+            .catch(err => {
+                console.error(err);
+                res.status(500).end();
+            });
     });
 
     router.post("/messages", function (req, res) {
