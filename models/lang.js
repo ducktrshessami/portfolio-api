3b8fed42661ba2d69ef2fa7d755deb2ca804a50c
Josh Fish <jwee761@gmail.com>
2021-03-24 16:47:11 -0700

    fixed nonexistent lang association

--- models/lang.js
+++ models/lang.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      models.Lang.belongsToMany(models.Project, { through: "ProjectLangs" });
+      // define association here
     }
   };
   Lang.init({
