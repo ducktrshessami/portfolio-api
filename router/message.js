2df6ef4794c39fe1f2da8790e24600ddb3d70174
ducktrshessami <jwee761@gmail.com>
2021-03-24 14:31:00 -0700

    handle mail content

--- router/message.js
+++ router/message.js
@@ -13,15 +13,22 @@ const mailer = createTransport({
 });
 
 function findError({ name, email, message }) {
-    if (!name) {
+    if (!process.env.MAILER_ENABLED) {
+        return "Messages are currently disabled";
+    }
+    if (!name.trim()) {
         return "Enter a name";
     }
-    if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
+    if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.trim())) {
         return "Enter a valid E-mail address";
     }
-    if (!message) {
+    if (!message.trim()) {
         return "Enter a message";
     }
+}
+
+function generateHtml(name, message) {
+
 }
 
 module.exports = function (router) {
@@ -31,8 +38,16 @@ module.exports = function (router) {
             res.status(400).end(validationError);
         }
         else {
+            let { name, email, message } = req.body;
+            name = name.trim();
+            email = email.trim();
+            message = message.trim();
             mailer.sendMail({
-
+                to: process.env.MAILER_DESTINATION,
+                replyTo: email,
+                subject: `Portfolio message from ${name}`,
+                text: `${name}\n${message}`,
+                html: generateHtml(name, message)
             })
                 .catch(err => {
                     console.error(err);
