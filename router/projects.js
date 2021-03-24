f962eaafc0b477bf79cb0fa6096cea9edc328ec7
Josh Fish <jwee761@gmail.com>
2021-03-24 16:29:29 -0700

    handle project list with gist

--- router/projects.js
+++ router/projects.js
@@ -1,44 +1,14 @@
-const db = require("../models");
-const auth = require("../middleware/auth");
+const { Octokit } = require("@octokit/rest");
 
-function insertProject(projectData) {
-    return db.Project.create(projectData)
-        .then(project => Promise.all((projectData.langs || []).map(lang => project.addLang(lang))));
-}
+const octokit = new Octokit({ auth: process.env.GIST_TOKEN });
 
 module.exports = function (router) {
     router.get("/projects", function (req, res) {
-        db.Project.findAll({
-            order: [["id", "asc"]],
-            attributes: ["title", "description", "url"],
-            include: {
-                model: db.Lang,
-                attributes: ["image"]
-            }
-        })
+        octokit.gists.get({ gist_id: process.env.GIST_ID })
             .then(data => res.status(200).json(data))
             .catch(err => {
                 console.error(err);
                 res.status(500).end();
-            });
-    });
-
-    router.post("/projects", auth, function (req, res) {
-        insertProject(req.body)
-            .then(() => res.status(200).end())
-            .catch(err => {
-                console.error(err);
-                res.status(500).end();
-            });
-    });
-
-    router.post("/projects/all", auth, function (req, res) {
-        db.Project.sync({ force: true })
-            .then(() => Promise.all(req.body.map(insertProject)))
-            .then(() => res.status(200).end())
-            .catch(err => {
-                console.error(err);
-                res.status(400).end();
             });
     });
 };
