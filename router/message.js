3691af62dd9dbfd86510814fd9b2e294e55dfdd9
ducktrshessami <jwee761@gmail.com>
2021-03-24 14:00:56 -0700

    create nodemailer transport

--- router/message.js
+++ router/message.js
@@ -1,43 +1,24 @@
-const db = require("../models");
-const auth = require("../middleware/auth");
+const { createTransport } = require("nodemailer");
+
+const transport = createTransport({
+    host: process.env.MAILER_HOST,
+    secure: true,
+    auth: {
+        type: "OAuth2",
+        user: process.env.MAILER_USER,
+        clientId: process.env.MAILER_ID,
+        clientSecret: process.env.MAILER_SECRET,
+        refreshToken: process.env.MAILER_REFRESH
+    }
+});
 
 module.exports = function (router) {
-    router.get("/messages", auth, function (req, res) {
-        db.Message.findAll({
-            order: [["createdAt", "asc"]],
-            attributes: ["name", "email", "message"]
-        })
-            .then(data => res.status(200).json(data))
-            .catch(err => {
-                console.error(err);
-                res.status(500).end();
-            });
-    });
-
-    router.post("/messages", function (req, res) {
+    router.post("/message", function (req, res) {
         db.Message.create(req.body)
             .then(data => res.status(200).json(data))
             .catch(err => {
                 console.error(err);
                 res.status(500).end();
-            });
-    });
-
-    router.delete("/messages/all", auth, function (req, res) {
-        db.Message.sync({ force: true })
-            .then(() => res.status(200).end())
-            .catch(err => {
-                console.error(err);
-                res.status(400).end();
-            });
-    });
-
-    router.delete("/messages/:id", auth, function (req, res) {
-        db.Message.destroy({ where: { id: Number(req.params.id) } })
-            .then(() => res.status(200).end())
-            .catch(err => {
-                console.error(err);
-                res.status(400).end();
             });
     });
 };
