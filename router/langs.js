11e484477ead04583a58a7deb0d25ebaceea4f5c
Josh Fish <jwee761@gmail.com>
2021-03-24 16:19:28 -0700

    allow bulk delete lang route

--- router/langs.js
+++ router/langs.js
@@ -54,4 +54,13 @@ module.exports = function (router) {
                 res.status(400).end();
             });
     });
+
+    router.delete("/langs/all", auth, function (req, res) {
+        db.Lang.sync({ force: true })
+            .then(() => res.status(200).end())
+            .catch(err => {
+                console.error(err);
+                res.status(500).end();
+            });
+    });
 };
