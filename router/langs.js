67b3c3b9207d57cc9b672f2473770e4da1e963d2
ducktrshessami <jwee761@gmail.com>
2021-03-24 19:46:17 -0700

    handle lang get

--- router/langs.js
+++ router/langs.js
@@ -4,6 +4,13 @@ const octokit = new Octokit({ auth: process.env.GIST_TOKEN });
 
 module.exports = function (router) {
     router.get("/langs", function (req, res) {
-
+        octokit.gists.get({ gist_id: process.env.GIST_ID })
+            .then(gist => gist.data.files["langs.json"].content)
+            .then(JSON.parse)
+            .then(data => res.status(200).json(data))
+            .catch(err => {
+                console.error(err);
+                res.status(500).end();
+            });
     });
 };
