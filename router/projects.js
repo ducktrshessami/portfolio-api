264202eed216cc79d0af20b7f8caa83d3923a922
ducktrshessami <jwee761@gmail.com>
2021-03-24 19:45:55 -0700

    handle gist res json

--- router/projects.js
+++ router/projects.js
@@ -4,7 +4,9 @@ const octokit = new Octokit({ auth: process.env.GIST_TOKEN });
 
 module.exports = function (router) {
     router.get("/projects", function (req, res) {
-        octokit.gists.get({ gist_id: process.env.GIST_PROJECTS })
+        octokit.gists.get({ gist_id: process.env.GIST_ID })
+            .then(gist => gist.data.files["projects.json"].content)
+            .then(JSON.parse)
             .then(data => res.status(200).json(data))
             .catch(err => {
                 console.error(err);
