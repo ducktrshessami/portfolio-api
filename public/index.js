c46db1a3bacdeb634f8b09283a050266cf4f429b
ducktrshessami <jwee761@gmail.com>
2021-03-30 20:29:33 -0700

    redirect instead of sendfile

--- public/index.js
+++ public/index.js
@@ -3,12 +3,11 @@ const path = require("path");
 
 const router = express.Router();
 const staticDir = path.resolve(__dirname, "static");
-const staticPage = path.join(staticDir, "index.html");
 
 router.use(express.static(staticDir));
 
 router.get("*", function (req, res) {
-    res.status(200).sendFile(staticPage);
+    res.redirect("/");
 });
 
 module.exports = router;
